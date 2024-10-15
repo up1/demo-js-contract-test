b0ca81f06fd6f57b03c760ec093baccad3ed9399
up1 <somkiat.p@gmail.com>
2024-10-15 23:17:46 +0700

    Add user not found case

--- contract-tests/demo01.pact.spec.js
+++ contract-tests/demo01.pact.spec.js
@@ -18,6 +18,7 @@ describe("Pact contract test between frontend and backend", () => {
 
   const user1 = { id: 1, name: "Somkiat Pui" };
   const SUCCESS_EXPECTED_BODY = MatchersV3.like(user1);
+  const NOTFOUND_EXPECTED_BODY = MatchersV3.like({ message: "User not found" });
 
   describe("when a call to the API is made to get user by id", () => {
     it("should receive the user details from the API", async () => {
@@ -37,5 +38,23 @@ describe("Pact contract test between frontend and backend", () => {
         expect(user).toEqual({ id: 1, name: "Somkiat Pui" });
       });
     });
+
+    it("should receive user not found message from the API", async () => {
+        const p = provider
+          .addInteraction()
+          .given("I not have a user in service")
+          .uponReceiving("a request for user with id 2")
+          .withRequest("GET", "/users/2")
+          .willRespondWith(404, (builder) => {
+            builder.headers({ "Content-Type": "application/json" });
+            builder.jsonBody(NOTFOUND_EXPECTED_BODY);
+          });
+  
+        return p.executeTest(async (mockserver) => {
+          process.env.BACKEND_URL = mockserver.url;
+          const user = await getUser(2);
+          expect(user).toEqual({ message: "User not found"});
+        });
+      });
   });
 });
